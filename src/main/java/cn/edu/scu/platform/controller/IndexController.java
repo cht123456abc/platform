78758d3d4f9e34558bca984a830adfcd8c5bb9c7
cht <916446837@qq.com>
2020-06-01 10:34:08 +0800

    静态资源
    Signed-off-by: cht <916446837@qq.com>

---
+++ src/main/java/cn/edu/scu/platform/controller/IndexController.java
@@ -0,0 +1,21 @@
+package cn.edu.scu.platform.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class IndexController {
+    Logger Log = LoggerFactory.getLogger(IndexController.class);
+
+        @RequestMapping({"/"," "})
+        public String index() {
+            Log.info("!!!!!!!!!!!!!!!!!");
+            return "index";
+        }
+
+
+}
